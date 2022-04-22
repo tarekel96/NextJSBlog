43cceb1e1745f532f3a8afbc454ee586eb835b58
Tarek <elhaj102@mail.chapman.edu>
2022-04-21 20:10:23 -0700

    completed the tutorial up to and including the dynamic routing section

--- nextjs-blog/postcss.config.js
+++ nextjs-blog/postcss.config.js
@@ -1,9 +1,6 @@
 // postcss.config.js
 module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {}
-	},
+	plugins: {},
 	content: [
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}'
